b41004585af130ec48fe781a7be40429a8013a4e
HomanHan <homanhan@outlook.com>
2024-03-23 22:52:52 +0800

    config

--- src/main/java/ltd/newbee/mall/common/Constants.java
+++ src/main/java/ltd/newbee/mall/common/Constants.java
@@ -13,8 +13,8 @@ package ltd.newbee.mall.common;
  */
 public class Constants {
     //public final static String FILE_UPLOAD_DIC = "/opt/image/upload/";//上传文件的默认url前缀，根据部署设置自行修改
-    public final static String FILE_UPLOAD_DIC = "/Users/liyaodong/Desktop/newbee-mall-master/src/main/resources/upload ";//上传文件的默认url前缀，根据部署设置自行修改
-
+    //public final static String FILE_UPLOAD_DIC = "/Users/liyaodong/Desktop/newbee-mall-master/src/main/resources/upload ";//上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "D:\\27690\\Documents\\MaterialX\\MaterialX\\src\\main\\resources\\upload\\";//上传文件的默认url前缀，根据部署设置自行修改
     public final static int INDEX_CAROUSEL_NUMBER = 5;//首页轮播图数量(可根据自身需求修改)
 
     public final static int INDEX_CATEGORY_NUMBER = 10;//首页一级分类的最大数量
