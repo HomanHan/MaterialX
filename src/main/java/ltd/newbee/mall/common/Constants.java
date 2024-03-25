d9520228e94d0b39f8fffba930940998e3672b1c
liyaodong <1320058298@qq.com>
2024-03-26 00:29:41 +0800

    logo

--- src/main/java/ltd/newbee/mall/common/Constants.java
+++ src/main/java/ltd/newbee/mall/common/Constants.java
@@ -13,7 +13,7 @@ package ltd.newbee.mall.common;
  */
 public class Constants {
     //public final static String FILE_UPLOAD_DIC = "/opt/image/upload/";//上传文件的默认url前缀，根据部署设置自行修改
-    public final static String FILE_UPLOAD_DIC = "/Users/liyaodong/Desktop/MaterialX/src/main/resources/upload ";//上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "/Users/liyaodong/Desktop/MaterialX/src/main/resources/upload/ ";//上传文件的默认url前缀，根据部署设置自行修改
     //public final static String FILE_UPLOAD_DIC = "D:\\27690\\Documents\\MaterialX\\MaterialX\\src\\main\\resources\\upload\\";//上传文件的默认url前缀，根据部署设置自行修改
     public final static int INDEX_CAROUSEL_NUMBER = 5;//首页轮播图数量(可根据自身需求修改)
 
